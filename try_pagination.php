1f15344f2b50651f92677cf2dabcc431dc1a4096
Squarer Liao <squarerliao@gmail.com>
2014-04-11 02:08:10 +0800

    initial

---
+++ try_pagination.php
@@ -0,0 +1,45 @@
+<?php
+include "pagination.class.php";
+
+$mysqli = new mysqli('localhost', 'user', 'pwd', 'database');
+$table = 'table';
+$result = $mysqli->query("select * from $table");
+
+if(!$result){
+	die("Query to show fields from table failed");
+}
+
+$page_size = 10;
+$pager = new pagination($result, $page_size);
+$result_per_page = $pager->get_result();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Try Pagination</title>
+</head>
+<body>
+<h1>Table: <?php echo $table ?></h1>
+<table border="1">
+<tr>
+<?php foreach ($result->fetch_fields() as $field): ?>
+	<td><?php echo $field->name; ?></td>
+<?php endforeach; ?>
+</tr>
+
+<?php foreach ($result_per_page as $row): ?>
+	<tr>
+	<?php foreach ($row as $cell): ?>
+		<td><?php echo $cell; ?></td>
+	<?php endforeach; ?>
+	</tr>
+<?php endforeach; ?>
+
+</table>
+
+<?php $pager->render() ?>
+
+</body>
+</html>
